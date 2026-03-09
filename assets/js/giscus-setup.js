16858e4c6441ef5696884a8640b70a4b90716faa
omin-Kwon <72242361+omin-Kwon@users.noreply.github.com>
2026-03-09 02:27:03 +0000

    Deploying to gh-pages from @ omin-Kwon/omin-Kwon.github.io@e16c3de1c23122736c55475dc19531e0fe171fbb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "omin-Kwon/ominkwon.github.io",
+    "data-repo": "omin-Kwon/omin-Kwon.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
