37d7ab89495e426629bb7643055c508859e06f3f
omin-Kwon <72242361+omin-Kwon@users.noreply.github.com>
2026-03-11 20:06:38 +0000

    Deploying to gh-pages from @ omin-Kwon/omin-kwon.github.io@d0efdbe9bd7ab44a5e7fa37a8369fd281dc3e88a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,6 +483,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=xkLAWloAAAAJ", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/ominkwon", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
